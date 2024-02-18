55c6e89ac1829e0fe68eb7ab07087cf67bb10b8f
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2024-02-18 12:11:00 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
 from flask import Flask
+from flask import redirect
 
 app = Flask(__name__)
-
 @app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+def web_redirect():
+    return redirect("https://yukthi.org", code=302)
+@app.route('/ar-vr')
+def tech_redirect():
+    return redirect("https://forms.gle/ZYk5jEryn1Vi6m7L9", code=302)
