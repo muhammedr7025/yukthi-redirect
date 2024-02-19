7b22f58d80abf326b7cba0a3e3742d36df3933bf
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2024-02-19 12:43:25 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def web_redirect():
     return redirect("https://yukthi.org", code=302)
 @app.route('/ar-vr')
-def tech_redirect():
+def ar_vr():
     return redirect("https://forms.gle/ZYk5jEryn1Vi6m7L9", code=302)
+@app.route('/robotics')
+def robotics():
+    return redirect("https://forms.gle/vsqG4RfCurwtfQ728", code=302)
+@app.route('/blockchain')
+def blockchain():
+    return redirect("https://forms.gle/F2zpbDCszadUTYTF8", code=302)
