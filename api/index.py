dfa82ea4b7851e0d39c7ff4693701989c266188e
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2024-02-20 10:08:38 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,3 +14,12 @@ def robotics():
 @app.route('/blockchain')
 def blockchain():
     return redirect("https://forms.gle/F2zpbDCszadUTYTF8", code=302)
+@app.route('/aiandllm')
+def aiandllm():
+    return redirect("https://forms.gle/PMw4exnJwYz2EmZf6", code=302)
+@app.route('/ui-ux')
+def ui_ux():
+    return redirect("https://forms.gle/Nkegh4t7Nc717dRY9", code=302)
+@app.route('/hardwarehacking')
+def hardwarehacking():
+    return redirect("https://forms.gle/WZQDeMzCBAFBgCEe8", code=302)
