3ff6ee139fdcd01dabb91c8fa68e563b5623f84a
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2024-02-29 12:17:08 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -35,3 +35,12 @@ def dprinting():
 @app.route('/proprogrammer')
 def proprogrammer():
     return redirect("https://docs.google.com/forms/d/e/1FAIpQLSd2ob9S1OC15fsXuKL_uWGhbhK2UtQCGrAcAeLIPHZ0XZ8DxA/viewform?embedded=true", code=302)
+@app.route('/blindprograming')
+def blindprograming():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSfCQddeZi6jbPDopaFgWaTIrO6CdFV3hg-g9RBIlSiRx4NPow/viewform?embedded=true", code=302)
+@app.route('/ctf')
+def ctf():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLScrxNp05Hb0tBPRBiFfHbZgKuxkPBrqB6O6aDmMBrKyJSSXpQ/viewform?embedded=true", code=302)
+@app.route('/keyboardmastery')
+def keyboardmastery():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeGkRodPuSC8oFenb33t0GWFtEEUAW69FfzGQTX5fohYF0dKg/viewform?embedded=true", code=302)
