a3a5f0e67970135b3b1a3ddb0d037b1f4eb211c8
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2024-03-07 14:04:23 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,31 +7,31 @@ def web_redirect():
     return redirect("https://yukthi.org", code=302)
 @app.route('/ar-vr')
 def ar_vr():
-    return redirect("https://forms.gle/ZYk5jEryn1Vi6m7L9", code=302)
+    return redirect("https://www.yukthi.org/events/arvr", code=302)
 @app.route('/robotics')
 def robotics():
-    return redirect("https://forms.gle/vsqG4RfCurwtfQ728", code=302)
+    return redirect("https://www.yukthi.org/events/robotics", code=302)
 @app.route('/blockchain')
 def blockchain():
-    return redirect("https://forms.gle/F2zpbDCszadUTYTF8", code=302)
+    return redirect("https://www.yukthi.org/events/blockchain", code=302)
 @app.route('/aiandllm')
 def aiandllm():
-    return redirect("https://forms.gle/PMw4exnJwYz2EmZf6", code=302)
+    return redirect("https://www.yukthi.org/events/aillm", code=302)
 @app.route('/ui-ux')
 def ui_ux():
-    return redirect("https://forms.gle/Nkegh4t7Nc717dRY9", code=302)
+    return redirect("https://www.yukthi.org/events/uiux", code=302)
 @app.route('/hardwarehacking')
 def hardwarehacking():
-    return redirect("https://forms.gle/WZQDeMzCBAFBgCEe8", code=302)
+    return redirect("https://www.yukthi.org/events/hwhacking", code=302)
 @app.route('/webdev')
 def webdev():
-    return redirect("https://forms.gle/iCnR5mVrTZmMiV7E9", code=302)
+    return redirect("https://www.yukthi.org/events/webdev", code=302)
 @app.route('/hackathon')
 def hackathon():
     return redirect("https://yukthi-hackathon.devfolio.co/", code=302)
 @app.route('/3dprinting')
 def dprinting():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLScwM3e3dw5b9fcIYBa3WHVeLecF-KPw35FUINUjoMnYD74srA/viewform?usp=sf_link", code=302)
+    return redirect("https://www.yukthi.org/events/3dprinting", code=302)
 @app.route('/proprogrammer')
 def proprogrammer():
     return redirect("https://docs.google.com/forms/d/e/1FAIpQLSd2ob9S1OC15fsXuKL_uWGhbhK2UtQCGrAcAeLIPHZ0XZ8DxA/viewform?embedded=true", code=302)
