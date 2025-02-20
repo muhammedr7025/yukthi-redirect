d90bd3693cc106c45b3dd83fbf649c9afc957f06
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2025-02-20 10:33:07 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,66 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def web_redirect():
     return redirect("https://yukthi.org", code=302)
-@app.route('/ar-vr')
+@app.route('/Deep-learning')
 def ar_vr():
-    return redirect("https://www.yukthi.org/events/arvr", code=302)
-@app.route('/robotics')
-def robotics():
-    return redirect("https://www.yukthi.org/events/robotics", code=302)
-@app.route('/blockchain')
-def blockchain():
-    return redirect("https://www.yukthi.org/events/blockchain", code=302)
-@app.route('/aiandllm')
-def aiandllm():
-    return redirect("https://www.yukthi.org/events/aillm", code=302)
-@app.route('/ui-ux')
-def ui_ux():
-    return redirect("https://www.yukthi.org/events/uiux", code=302)
-@app.route('/hardwarehacking')
-def hardwarehacking():
-    return redirect("https://www.yukthi.org/events/hwhacking", code=302)
-@app.route('/webdev')
-def webdev():
-    return redirect("https://www.yukthi.org/events/webdev", code=302)
-@app.route('/hackathon')
-def hackathon():
-    return redirect("https://yukthi-hackathon.devfolio.co/", code=302)
-@app.route('/3dprinting')
-def dprinting():
-    return redirect("https://www.yukthi.org/events/3dprinting", code=302)
-@app.route('/proprogrammer')
-def proprogrammer():
-    return redirect("https://www.yukthi.org/events/propgmmer", code=302)
-@app.route('/blindprograming')
-def blindprograming():
-    return redirect("https://www.yukthi.org/events/blindpgm", code=302)
-@app.route('/ctf')
-def ctf():
-    return redirect("https://www.yukthi.org/events/ctf", code=302)
-@app.route('/keyboardmastery')
-def keyboardmastery():
-    return redirect("https://www.yukthi.org/events/kbmastery", code=302)
-@app.route('/webhunt')
-def webhunt():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSfu7FqJTc-eksIUrLXStNLFEyTMdOPARg3-1zqC-LbG0J_i6g/viewform?embedded=true", code=302)
-@app.route('/circuitdebugging')
-def circuitdebugging():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSc47pP1PX5qAxjKbhrms6ojZyCmF01Td0GDFbCng4RO09PTAg/viewform?embedded=true", code=302)
-@app.route('/spanathon')
-def spanathon():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeLDGKur7k4EQxdoWyZXDIolrSbOkJL51vhO6rpDJ5e9YkW0Q/viewform?embedded=true", code=302)
-@app.route('/segfault')
-def segfault():
-    return redirect("https://app.formbricks.com/eEXUTDkfk4", code=302)
-@app.route('/terminusrobo')
-def terminusrobo():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeeK6ur-MEOMhl2GPADybgF3rXFqdqocCFsHGqOs-cImH_TsQ/viewform?usp=pp_url", code=302)
-@app.route('/flutter')
-def flutter():
-    return redirect("https://www.yukthi.org/events/flutter", code=302)
-@app.route('/mixdesign')
-def mixdesign():
-    return redirect("https://forms.gle/AuXocomjonwfDzuU9", code=302)
-@app.route('/paperpinnacle')
-def paperpinnacle():
-    return redirect("https://forms.gle/PxFfQKkXA3duKU2u9", code=302)
+    return redirect("https://forms.gle/dgGsjh4PJ83WaYiv7", code=302)
+@app.route('/Game-developement')
+def ar_vr():
+    return redirect("https://forms.gle/itc3wofLT7kKpTjZ6", code=302)
