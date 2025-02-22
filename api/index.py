231ad7d2005be323351f02f46118ca82380ea25b
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2025-02-22 13:57:01 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import redirect
 
-app = Flask(__name__)
+app = Flask(_name_)
 @app.route('/')
 def web_redirect():
     return redirect("https://yukthi.org", code=302)
@@ -15,3 +15,18 @@ def game_dev_redirect():
 @app.route('/yudhya-hackathon')
 def yudhya_hackathon_redirect():
     return redirect("https://yudhya.devfolio.co/overview", code=302)
+@app.route('/Rpa')
+def rpa_redirect():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSc9UjbDdzI2qGbIu7snO8nfPIrNgeD8vVaYTW1cSxbkzL9JHw/viewform?usp=header", code=302)
+@app.route('/Ui-Ux')
+def ui_ux_redirect():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSfKvcyB9cPBnjHmVEwGm-JuJf4yey74yzCyg52iTobz-BQF1g/viewform?usp=header", code=302)
+@app.route('/Drone-scope')
+def drone_scope_redirect():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSeDR7oeqkjqsfV9EBGWa5wNGlXKFZlhlLolvvLjKi-M_iJYwg/viewform?usp=header", code=302)
+@app.route('/unreal-engine')
+def unreal_engine_redirect():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLScQiolFj4A-PX6iywle1FkijByfzPV8a9dRjA5AXkg5I2mByg/viewform?usp=header", code=302)
+@app.route('/Ai-Llm')
+def ai_llm_redirect():
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSdLw2aAQQO64Seicvg-h-QMxM_SWAEDjS5Kd5dnT6t1mb-M_g/viewform?usp=header", code=302)
